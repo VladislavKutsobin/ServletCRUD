d8e116f4f4794de0a7915b0694cc2fe8329bd470
Vladislav Kutsobin <kutsobin_vladislav@rambler.ru>
2019-05-31 23:27:46 +0300

    Created general repository interface

---
+++ src/main/java/app/repository/GenericRepository.java
@@ -0,0 +1,11 @@
+package app.repository;
+
+import java.util.List;
+
+public interface GenericRepository<T, ID> {
+    void save(T t);
+    List<T> findAll();
+    void update(T t);
+    void delete(ID id);
+    T getById(ID id);
+}
